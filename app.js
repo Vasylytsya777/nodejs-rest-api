27727721b5ea1c2ae2c6cc3e38008ce76b5b74ff
Igor Vasylytsya <vasylytsya.igor@gmail.com>
2021-04-15 22:13:56 +0300

    finish 5 hw

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
-const contactsRouter = require("./routes/api/contacts");
+const contactsRouter = require("./routes/api/contactsRoutes");
+const usersRouter = require("./routes/api/usersRoutes");
 
 const app = express();
 
@@ -13,13 +15,18 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
+// eslint-disable-next-line node/no-path-concat
+app.use("/avatars", express.static(path.join(__dirname + "/public/avatars")));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  res.status(500).json({
+    message: err.message,
+  });
 });
 
 module.exports = app;
